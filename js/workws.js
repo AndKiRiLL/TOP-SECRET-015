b27436dfa591d95ecf3b112807cc0bcd30c11cb1
AndKiRiLL <144206743+AndKiRiLL@users.noreply.github.com>
2024-02-13 01:22:25 +0300

    Add files via upload

---
+++ js/workws.js
@@ -0,0 +1,22 @@
+/*
+input.value = "Новое значение";
+textarea.value = "новый текст";
+
+input.checked = true; // для чекбоксов и переключателей
+*/
+
+//let option = new Option('Текст', 'value');
+// создаст <option value="value">Текст</option>
+
+//let option = new Option("Текст", "value", true, true);
+
+// let form = document.createElement('form');
+// form.action = 'https://google.com/search';
+// form.method = 'GET';
+
+// form.innerHTML = '<input name="q" value="test">';
+
+// перед отправкой формы, её нужно вставить в документ
+// document.body.append(form);
+
+// form.submit();
